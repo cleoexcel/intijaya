868e273643fa94a670fa0860c83e690e398f9edb
cleoexcel <cleoxclln@gmail.com>
2024-09-10 09:31:02 +0700

    Fix : Change the NPM to Class name

--- main/urls.py
+++ main/urls.py
@@ -1,8 +1,9 @@
+from django.shortcuts import render
 from django.urls import path
 from main.views import show_main
 
 app_name = 'main'
 
 urlpatterns = [
-    path('', show_main, name='show_main'),
+    path('halo', show_main, name='show_main'),
 ]
--- main/views.py
+++ main/views.py
@@ -2,12 +2,8 @@ from django.shortcuts import render
 
 def show_main(request):
     context = {
-        'npm' : '2306244886',
-        'nama': 'Cleo Excellen Iskandar',
-    
+        'kelas' : 'PBP C',
+        'nama': 'Cleo Excellen Iskandar'
     }
 
     return render(request, "main.html", context)
-
-
-# Create your views here.
